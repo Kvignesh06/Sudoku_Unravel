cd18fff33feae85df86130980b9dd69e40b6d407
Kvignesh06 <160007753+Kvignesh06@users.noreply.github.com>
2024-08-13 07:57:56 +0530

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,5 +1,34 @@
 import numpy as np
 
+def possible(grid, row, column, number):
+    for i in range(9):
+        if grid[row][i] == number:
+            return False
+    for i in range(9):
+        if grid[i][column] == number:
+            return False
+    x = (column // 3) * 3
+    y = (row // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if grid[y + i][x + j] == number:
+                return False
+    return True
+
+def solve(grid):
+    for row in range(9):
+        for column in range(9):
+            if grid[row][column] == 0:
+                for number in range(1, 10):
+                    if possible(grid, row, column, number):
+                        grid[row][column] = number
+                        if solve(grid):
+                            return True
+                        grid[row][column] = 0
+                return False  # Return False if no number can be placed
+    print(np.matrix(grid))
+    return True
+
 grid = [[9,0,6,0,7,0,4,0,3],
         [0,0,0,4,0,0,2,0,0],
         [0,7,0,0,2,3,0,1,0],
@@ -10,42 +39,5 @@ grid = [[9,0,6,0,7,0,4,0,3],
         [0,0,7,0,0,5,0,0,0],
         [4,0,5,0,1,0,7,0,8]]
 
-def possible(row, column, number):
-    global grid
-    #Is the number appearing in the given row?
-    for i in range(0,9):
-        if grid[row][i] == number:
-            return False
-
-    #Is the number appearing in the given column?
-    for i in range(0,9):
-        if grid[i][column] == number:
-            return False
-    
-    #Is the number appearing in the given square?
-    x = (column // 3) * 3
-    y = (row // 3) * 3
-    for i in range(0,3):
-        for j in range(0,3):
-            if grid[y+i][x+j] == number:
-                return False
-
-    return True
-
-def solve():
-    global grid
-    for row in range(0,9):
-        for column in range(0,9):
-            if grid[row][column] == 0:
-                for number in range(1,10):
-                    if possible(row, column, number):
-                        grid[row][column] = number
-                        solve()
-                        grid[row][column] = 0
-
-                return
-      
-    print(np.matrix(grid))
-    input('Press ENTER to see more possible solutions')
-
-solve()
+if not solve(grid):
+    print("No solution exists.")
